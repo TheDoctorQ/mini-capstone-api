50b058cd7dd7f965d93914d3694e1356492a2692
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-13 18:09:44 -0600

    adds post command

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,8 +7,18 @@ class ProductsController < ApplicationController
 
   def show
     product = Product.find_by(id: params[:id])
-    render json:product
+    render json: product
   end
 
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    product.save
+    render json: product
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
-
+  post "products" => "products#create"
+ 
 end
