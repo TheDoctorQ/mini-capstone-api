0c45c26a2e0ee04bc09a6a2f293cd275d991cbec
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-16 10:56:31 -0600

    reverts to pre-migration change

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,34 +1,36 @@
 class ProductsController < ApplicationController
 
   def index
-    products = Product.all
-    render json: products
+    @products = Product.all
+    # render json: products
+    render template: "products/index"
   end
 
   def show
     product = Product.find_by(id: params[:id])
-    render json: product
+    # render json: product
+    render template: "products/show"
   end
 
   def create
-    product = Product.new(
+    @product = Product.new(
       name: params[:name],
       price: params[:price],
       image_url: params[:image_url],
       description: params[:description]
     )
-    product.save
-    render json: product
+    @product.save
+    render template: "products/show"
   end
 
   def update
-    product = Product.find_by(id: params[:id])
-    product.name = params[:name]
-    product.price = params[:price]
-    product.image_url = params[:image_url]
-    product.description = params[:description]
-    product.save
-    render json: product
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image_url = params[:image_url]
+    @product.description = params[:description]
+    @product.save
+    render template: "products/show"
   end
 
   def destroy
