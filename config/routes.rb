0f54f58bd7173314829ac29d2045ca529e924d92
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-22 13:38:45 -0600

    adds users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
-  post "products" => "products#create"
+  post "/products" => "products#create"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
-  
+
+  post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
+
+  get "/orders" => "orders#index"
   post "/orders" => "orders#create"
   get "/orders/:id" => "orders#show"
-  get "/orders" => "orders#index"
-
-
+  
 end
