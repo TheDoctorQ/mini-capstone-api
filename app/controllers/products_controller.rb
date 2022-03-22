d3a41124775eb83001506b86595297b956c10914
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-22 11:03:47 -0600

    adds quantity

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,12 +33,11 @@ class ProductsController < ApplicationController
   end
 
 
-
   def update
     product = Product.find_by(id: params[:id])
     product.name = params[:name]
     product.price = params[:price]
-    product.image_url = params[:image_url]
+    # product.image_url = params[:image_url]
     product.description = params[:description]
     product.save
     render template: "products/show"
---
+++ db/migrate/20220322165423_change_inventory_to_quantity.rb
@@ -0,0 +1,11 @@
+class ChangeInventoryToQuantity < ActiveRecord::Migration[7.0]
+  
+  def change
+    remove_column :products, :inventory
+  end
+  
+  def change
+    add_column :products, :quantity, :integer
+  end
+  
+end
